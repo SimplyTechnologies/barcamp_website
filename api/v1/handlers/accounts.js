bc2f89ff063d2ef19781d3faa467b176a0ba15bd
Mikael Manukyan <arm.localhost@gmail.com>
2015-05-18 17:54:21 +0400

    feat(accounts): add validation to signIn

--- api/v1/handlers/accounts.js
+++ api/v1/handlers/accounts.js
@@ -6,6 +6,14 @@ function AccountsHandler(accounts) {
 }
 
 AccountsHandler.prototype.signIn = function(req, res, next) {
+    req.checkBody('username', 'required').notEmpty();
+    req.checkBody('password', 'required').notEmpty();
+
+    var errors = req.validationErrors();
+    if (errors) {
+        return res.status(400).send(errors);
+    }
+
     return this.service.signIn(req.body.username, req.body.password)
         .then(function (user) {
             req.signin(user._id);
