c8b44ca65e5c23bf262c7e20e48ce5ea12e985ba
andantonyan <andantonyan@gmail.com>
2015-05-27 19:17:07 +0400

    remove fake date (timetable)

--- app/scripts/controllers/TimetableController.ts
+++ app/scripts/controllers/TimetableController.ts
@@ -26,112 +26,17 @@ module barcamp {
             this.scope.rooms = ['Big Hall', 'W1', 'W2', 'P1', 'P2'];
             this.scope.roomKeys = ['big_hall', 'w1', 'w2', 'p1', 'p2'];
             this.scope.days = [1, 2];
-            this.scope.eventDate = {1: new Date('2015-05-30'), 2: new Date('2015-06-01')}; //temporary
+            this.scope.eventDate = {1: new Date('2015-05-30'), 2: new Date('2015-05-31')}; //temporary
             this.scope.selectedDay = 1;
             this.getEventByDay(this.scope.selectedDay);
         }
 
         getEventByDay(day: number): void {
             var startTime = this.moment(this.scope.eventDate[day]).startOf('day').hour(9).minute(0);
-
+            this.scope.scheduleRooms = {};
             this.scheduleService.getByDay(day)
                 .then((scheduleList) => {
                     scheduleList = scheduleList.data;
-                    scheduleList = [
-                        {
-                            bg_image_url: "http://api.barcamp.am/i/speaker/1_samvel.png",
-                            room: "W1",
-                            en: {
-                                speaker: "Samvel Martirosyan",
-                                topic: "Data Verification"
-                            },
-                            hy: {
-                                speaker: "Սամվել Մարտիրոսյան",
-                                topic: "Դաթա Վերիֆիքեյշն"
-                            },
-                            time_from: {
-                                date: "2015-05-27 09:30:42.00",
-                                timezone_type: 3,
-                                timezone: "America\/Chicago"
-                            },
-                            time_to: {
-                                date: "2015-05-27 11:00:42.00",
-                                timezone_type: 3,
-                                timezone: "America\/Chicago"
-                            }
-                        },
-
-                        {
-                            bg_image_url: "http://api.barcamp.am/i/speaker/1_samvel.png",
-                            room: "W1",
-                            en: {
-                                speaker: "Foo Bar",
-                                topic: "Dvdfgdfg Verification"
-                            },
-                            hy: {
-                                speaker: "Սամվել Մարտիրոսյան",
-                                topic: "Դաթա Վերիֆիքեյշն"
-                            },
-                            time_from: {
-                                date: "2015-05-30 12:30:42.00",
-                                timezone_type: 3,
-                                timezone: "America\/Chicago"
-                            },
-                            time_to: {
-                                date: "2015-05-30 13:20:42.00",
-                                timezone_type: 3,
-                                timezone: "America\/Chicago"
-                            }
-                        },
-
-
-                        {
-                            bg_image_url: "http://api.barcamp.am/i/speaker/1_samvel.png",
-                            room: "Big Hall",
-                            en: {
-                                speaker: "gfghggh fggfh",
-                                topic: "Data Verification"
-                            },
-                            hy: {
-                                speaker: "Սամվել Մարտիրոսյան",
-                                topic: "ghfgh"
-                            },
-                            time_from: {
-                                date: "2015-05-27 16:00:42.00",
-                                timezone_type: 3,
-                                timezone: "America\/Chicago"
-                            },
-                            time_to: {
-                                date: "2015-05-27 17:20:42.00",
-                                timezone_type: 3,
-                                timezone: "America\/Chicago"
-                            }
-                        },
-
-
-                        {
-                            bg_image_url: "http://api.barcamp.am/i/speaker/1_samvel.png",
-                            room: "p1",
-                            en: {
-                                speaker: "gfghggh fggfh",
-                                topic: "Data Verification"
-                            },
-                            hy: {
-                                speaker: "Սամվել Մարտիրոսյան",
-                                topic: "ghfgh"
-                            },
-                            time_from: {
-                                date: "2015-05-27 10:00:00",
-                                timezone_type: 3,
-                                timezone: "America\/Chicago"
-                            },
-                            time_to: {
-                                date: "2015-05-27 12:00:00",
-                                timezone_type: 3,
-                                timezone: "America\/Chicago"
-                            }
-                        }
-                    ];
 
                     var scheduleRooms = {
                         big_hall : [],
